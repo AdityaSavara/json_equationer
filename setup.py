151016b14d4e378d144a4610486de135e30a0324
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-05-28 02:19:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup, Command
 NAME = 'json_equationer'
 DESCRIPTION = 'A package for creating json equation records and evaluating them.'
 URL = 'https://github.com/AdityaSavara/json_equationer'
-EMAIL = 'AditySavara2008@u.northwestern.edu'  
+EMAIL = 'AdityaSavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.0.0'
 VERSION = '2.1'
