f6fbc6ef980ba5881457fbd3513ec00ff5d6d80c
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-08-01 19:25:02 -0400

    Version 2.2 with docstrings

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/AdityaSavara/json_equationer'
 EMAIL = 'AdityaSavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '2.1'
+VERSION = '2.2'
 LICENSE = 'Unlicense'
 
 # What packages are required for this module to be executed?
